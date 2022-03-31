fecb79001e0b9dd2d0316305d95d434f6c1c7fbe
eusebiops14 <64171953+eusebiops14@users.noreply.github.com>
2022-03-31 08:27:42 -0300

    5.06

--- cli.js
+++ cli.js
@@ -2,12 +2,17 @@
 //const pegaArquivo = require('./index');
 import  chalk  from 'chalk';
 import pegaArquivo from './index.js';
+import validaURLs from './http-validacao.js';
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo) {
     const resultado = await pegaArquivo(caminhoDeArquivo[2]);
-    console.log(chalk.yellow('lista de links'), resultado);
+    if(caminho[3] === 'validar') {
+        console.log(chalk.yellow('links validados'), await validaURLs(resultado));
+    }else {
+        console.log(chalk.yellow('lista de links'), resultado);
+    }
 }
 
 processaTexto(caminho);
---
+++ http-validacao.js
@@ -0,0 +1,22 @@
+//Instalar node-fetch
+import fetch from "node-fetch";
+
+async function checaStatus(arrayURLs) {
+    //promisses async await
+    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
+        const res = await fetch(url);
+        return res.status;
+    }));
+    return arrayStatus;
+}
+
+
+function geraArrayDeURLs(arrayLinks) {
+   return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
+}
+
+export default async function validaURLs(arrayLinks) {
+   const links = geraArrayDeURLs(arrayLinks);
+   const statusLink = await checaStatus(links);
+   return statusLink;
+}
